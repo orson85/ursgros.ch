15e102c88180c32ef20574adca33caea6355e75d
Urs Grossenbacher <ursgros@gmail.com>
2022-07-23 09:35:16 +0200

    navBurger created

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { navBarScroll } from "./modules/navBar.js";
+import { navBarScroll, toggleBurger } from "./scripts/navBar.js";
 
-
+//Navigation
 window.onscroll = () => { navBarScroll() }
+document.getElementById("toggle").addEventListener("click", () => { toggleBurger() }) //FOR TESTING
+
+
--- scripts/navBar.js
+++ scripts/navBar.js
@@ -4,20 +4,28 @@
 // https://css-tricks.com/ease-out-in-ease-in-out/
 // https://stackoverflow.com/questions/31223341/detecting-scroll-direction
 var lastScrollTop = 0;
+var navBar = document.getElementById("nav");
+var navBurger = document.getElementById("navBurger");
 
 function navBarScroll(){
    var st = window.pageYOffset || document.documentElement.scrollTop;
    if (st > lastScrollTop){
       console.log("dwn")
-      document.querySelector("nav").classList.remove("appear")
-      document.querySelector("nav").classList.add("disappear")
+      navBar.classList.remove("appear")
+      navBar.classList.add("disappear")
    } else {
     console.log("up")
-    document.querySelector("nav").classList.add("appear")
-    document.querySelector("nav").classList.remove("disappear")
-
+    navBar.classList.add("appear")
+    navBar.classList.remove("disappear")
    }
    lastScrollTop = st <= 0 ? 0 : st; 
 };
 
-export { navBarScroll };
+function toggleBurger() {
+   navBurger.classList.toggle("burger-close");
+   navBurger.classList.toggle("burger-open");
+   console.log("Wee!")
+   
+}
+
+export { navBarScroll, toggleBurger };
